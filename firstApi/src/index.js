43d37b039705a6bc769fa7ee2e0cb6d024011a10
Luis Guilherme C G <51034261+luisgcostaguedes@users.noreply.github.com>
2023-05-30 20:31:13 +0000

    test

--- firstApi/src/index.js
+++ firstApi/src/index.js
@@ -4,7 +4,7 @@ const { report } = require("process");
 const server = http.createServer((request, response) => {
   console.log(`Request method`);
   response.writeHead(200, { "Content-Type": "text/html" });
-  response.end("<h1>Hello world!</h1>");
+  response.end("<h1>Hello world com batata!</h1>");
 });
 
 server.listen(3000, () =>
