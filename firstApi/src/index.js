88c2085b1935a80f094c35bebaf02054aa30dc46
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-05-30 21:28:38 -0300

    up

--- firstApi/src/index.js
+++ firstApi/src/index.js
@@ -2,9 +2,15 @@ const http = require("http");
 const { report } = require("process");
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method`);
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.end("<h1>Hello world com batata!</h1>");
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+
+  if (request.url === "/users" && request.method === "GET") {
+    response.writeHead(200, { "Content-Type": "aplication/json" });
+    response.end("<h1>Hello world!</h1>");
+  }
+
+  //response.writeHead(200, { "Content-Type": "text/html" });
+  //response.end("<h1>Hello world!</h1>");
 });
 
 server.listen(3000, () =>
