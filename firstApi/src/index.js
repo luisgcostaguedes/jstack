c2e7e5838c8374e5c487194d3e2f4d993a5b4233
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-05-31 19:36:03 -0300

    ajust

--- firstApi/src/index.js
+++ firstApi/src/index.js
@@ -1,15 +1,31 @@
 const http = require("http");
+const url = require(´url´);
 const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
+  const parsedUrl = url.parse(request.url);
+  console.log(parsedUrl);
+
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
-  if (request.url === "/users" && request.method === "GET") {
-    userController.listUsers(request, response);
+  const route = routes.find(
+    (routeObj) =>
+      routeObj.endpoint === request.url && routeObj.method === request.method
+  );
+
+  if (route) {
+    route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
     response.end(`Cannot ${request.method} ${request.url}`);
   }
+
+  /*
+  if (request.url === "/users" && request.method === "GET") {
+    userController.listUsers(request, response);
+  } else {
+    
+  }*/
 });
 
 server.listen(3000, () =>
