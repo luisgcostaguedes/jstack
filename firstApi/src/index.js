499dd75947fded5c39457f440a6a0041a0fcac41
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-05-31 07:01:11 -0300

    up mac

--- firstApi/src/index.js
+++ firstApi/src/index.js
@@ -1,16 +1,17 @@
 const http = require("http");
-const { report } = require("process");
+
+const users = require("./mocks/users");
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
   if (request.url === "/users" && request.method === "GET") {
     response.writeHead(200, { "Content-Type": "aplication/json" });
-    response.end("<h1>Hello world!</h1>");
+    response.end(JSON.stringify(users));
+  } else {
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.end(`Cannot ${request.method} ${request.url}`);
   }
-
-  //response.writeHead(200, { "Content-Type": "text/html" });
-  //response.end("<h1>Hello world!</h1>");
 });
 
 server.listen(3000, () =>
---
+++ firstApi/src/mocks/users.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Luis",
+  },
+
+  {
+    id: 2,
+    name: "Batata",
+  },
+
+  {
+    id: 3,
+    name: "Couve",
+  },
+];
