543b4e627658dc339f839b313dcec5381395107b
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-06-02 19:04:01 -0300

    melhorado

---
+++ firstApi/mocks/users.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        id: 1,
+        name:'Luis',
+
+    },
+
+
+    {
+        id: 2,
+        name:'Batata',
+
+
+    },
+
+
+    {
+        id: 3,
+        name:'Zé',
+
+
+    }
+]
--- firstApi/src/index.js
+++ firstApi/src/index.js
@@ -1,47 +1,17 @@
 const http = require("http");
-const { URL } = require("url");
-
-const routes = require("./routes");
-
+const users = require("../mocks/users");
 const server = http.createServer((request, response) => {
-  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
-  console.log(
-    `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
-  );
-
-  let { pathname } = parsedUrl;
-  let id = null;
-
-  const splitEndpoint = pathname.split("/").filter(Boolean);
-
-  if (splitEndpoint.length > 1) {
-    pathname = `/${splitEndpoint[0]}/:id`;
-    id = splitEndpoint[1];
-  }
-  console.log(splitEndpoint);
-
-  const route = routes.find(
-    (routeObj) =>
-      routeObj.endpoint === pathname && routeObj.method === request.method
-  );
-
-  if (route) {
-    request.query = parsedUrl.query;
-    request.params = { id };
-    response.send = (statusCode, body) => {
-      response.writeHead(statusCode, { "Content-Type": "aplication/json" });
-      response.end(JSON.stringify({ body }));
-    };
-    route.handler(request, response);
+  if (request.url === "/users" && request.method === "GET") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(users));
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
-    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
+    response.end(`Cannot ${request.method} ${request.url}`);
   }
 });
 
 server.listen(3000, () =>
-  console.log("🔥Server started at http://localhost:3000")
+  console.log("🔥 Server started at http://localhost:3000")
 );
-
-/*sevidor inicial que no qual já temos como parametro as passagens de query params*/
