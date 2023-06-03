4df0b70f6ff51c6c715b9b59f8557d3f49ef45e3
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-06-02 22:17:23 -0300

    corrigido

--- firstApi/src/index.js
+++ firstApi/src/index.js
@@ -1,16 +1,23 @@
 const http = require("http");
+const { URL } = require("url");
 
 const routes = require("../routes");
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+
+  console.log(
+    `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
+  );
 
   const route = routes.find(
     (routeObj) =>
-      routeObj.endpoint === request.url && routeObj.method === request.method
+      routeObj.endpoint === parsedUrl.pathname &&
+      routeObj.method === request.method
   );
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
     route.hendler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
