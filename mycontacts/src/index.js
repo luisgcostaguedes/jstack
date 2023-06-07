7894d9af48d08190fc6d75b9db5fcf97c91148a6
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-06-07 16:05:58 -0300

    edit

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,9 +1,7 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
-app.get("/", (request, response) => {
-  response.send("Hello World!!");
+app.get('/', (request, response) => {
+  response.send('Hello World!!');
 });
-app.listen(3000, () =>
-  console.log("🔥 Server started at http://localhost:3000")
-);
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
