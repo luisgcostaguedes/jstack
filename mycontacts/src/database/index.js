c806b8631b7c259f69b3be1cd5fecb27546eb779
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-06-25 18:48:42 -0300

    depurar create

--- mycontacts/src/database/index.js
+++ mycontacts/src/database/index.js
@@ -11,9 +11,7 @@ const client = new Client({
 
 client.connect();
 
-async function Query(query) {
-  const { rows } = await client.query(query);
+exports.query = async (query, values) => {
+  const { rows } = await client.query(query, values);
   return rows;
-}
-
-Query('SELECT * FROM contacts').then(console.log);
+};
