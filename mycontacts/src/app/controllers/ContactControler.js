25023eb2985e5d9e7715103fb466fdf98b208e86
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-07-09 18:29:29 -0300

    tudo ok

--- mycontacts/src/app/controllers/ContactControler.js
+++ mycontacts/src/app/controllers/ContactControler.js
@@ -65,12 +65,6 @@ class ContactControler {
 
   async delete(request, response) { // deletar novo registro
     const { id } = request.params;
-    const contact = await ContactsRepository.findById(id);
-
-    if (!contact) {
-      // 404
-      return response.status(404).json({ error: 'User not found' });
-    }
     await ContactsRepository.delete(id);
     response.sendStatus(204);
   }
