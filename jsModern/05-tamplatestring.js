46b7817d92ad7a377040ad2ccc7c278f0b292f3a
luisgcostaguedes <luisgcostaguedes@gmail.com>
2023-07-18 20:20:11 -0300

    vai curintia

---
+++ jsModern/05-tamplatestring.js
@@ -0,0 +1,11 @@
+// uso de qualquer 'aspas' é valido para a mesma coisa, agora quando usamos a `crase` podemos interpolar tudo no JS
+
+
+const exemploDeLinha = 'uma linha apenas';
+
+const linhas = `aqui podemos dar enter e vai ser respeitado o mesmo
+segue o fioooo`;
+
+console.log(exemploDeLinha);
+
+console.log(linhas);
